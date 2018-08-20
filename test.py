307e7c438a4e3048b17c3f4a04acea2dc5e26d31
David Banks <amoebae@gmail.com>
2018-08-20 17:13:37 +0100

    read expected data and compare results

--- test.py
+++ test.py
@@ -1,6 +1,18 @@
 # test skeleton
 
 import main
+import json
+
+EXPECTED_PATHS = {
+    'female_embedding': 'expected_female_embeddings.json',
+    'male_embedding': 'expected_male_embeddings.json'
+}
+
+expected_data = {}
+
+for k, v in EXPECTED_PATHS.items():
+    with open("resources/%s" % v, 'r') as f:
+        expected_data[k] = json.load(f)
 
 def inc(x):
     return x + 1
@@ -9,4 +21,8 @@ def test_answer():
     assert inc(3) == 4
 
 def test_main():
-    main.main(limit=10)
+    result = main.main(limit=10)
+
+    assert result['female_embedding'] == expected_data['female_embedding']
+    assert result['male_embedding'] == expected_data['male_embedding']
+
